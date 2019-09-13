13f16c356c5ba0cac6fdce65a384dd883f8cd4c3
EC2 Default User <ec2-user@ip-172-31-21-148.us-west-2.compute.internal>
2019-09-13 15:38:21 +0000

    initial commit

---
+++ storage/main.tf
@@ -0,0 +1,15 @@
+#Create a random id (s3 must be globally unique)
+resource "random_id" "tf_bucket_id" {
+  byte_length = 2
+}
+
+#Create bucket
+resource "aws_s3_bucket" "tf_code" {
+  bucket        = "${var.project_name}-${random_id.tf_bucket_id.dec}"
+  acl           = "private"
+  force_destroy = true
+
+  tags {
+    Name = "tf_bucket"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "aws_region" {}
+variable "project_name" {}
+
+#Networking
+variable "vpc_cidr" {}
+
+variable "public_cidrs" {
+  type = "list"
+}
+
+variable "accessip" {}
+
+#Compute
+variable "key_name" {}
+
+variable "public_key_path" {}
+variable "server_instance_type" {}
+
+variable "instance_count" {
+  default = 1
+}
